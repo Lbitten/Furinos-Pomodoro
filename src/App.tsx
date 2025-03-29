84e4ccf94069e80e2d79d4613c2f769940a58315
Luan Bittencourt <luanpsn18@gmail.com>
2025-03-29 17:57:43 -0300

    Criando APP.tsx e escolhendo melhor forma de usar o export

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+export function App() {
+  console.log("AAOOO");
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Para a Mel</h1>
+
+      <h2>
+        Vida, eu te amo em todos os momentos do meu dia. Quando acordo e tomo
+        minha primeira xícara de café você está em meus pensamentos. Quando
+        trabalho, busco conforto em nossos momentos juntos. Quando vou dormir,
+        lembro que tenho alguém para compartilhar a cama. Te amo de janeiro a
+        janeiro, do primeiro raiar de sol até a última luz do luar.
+      </h2>
     </>
   );
 }
-
-export default App;
