c0a3669304d530bf7e2745ebbf219e495bb1f317
Luan Bittencourt <luanpsn18@gmail.com>
2025-04-03 21:00:43 -0300

    Criando estrutura do formulario

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,27 @@ export function App() {
       <Container>
         <CountDown />
       </Container>
-      <Container>Form</Container>
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <label htmlFor="input">Task</label>
+            <input id="input" type="text" />
+          </div>
+
+          <div className="formRow">
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing.</span>
+          </div>
+
+          <div className="formRow">
+            <span>Ciclos</span>
+            <span>O O O O O</span>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
       <Container>Footer</Container>
     </>
   );
